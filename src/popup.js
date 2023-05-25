83ac4fc0bf6050d29f67af52ef716415f18b7b41
Harshal Mahajan <104901466+harshm1225@users.noreply.github.com>
2023-05-25 14:34:24 +0530

    Add files via upload

---
+++ src/popup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./popup.css";
+const Popup = () => {
+  return (
+    <div className="popup-container">
+      <div className="popup">
+        <span>Please select a rating before submitting.</span>
+      </div>
+    </div>
+  );
+};
+export default Popup;
